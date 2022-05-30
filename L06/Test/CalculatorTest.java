69a84229305096f668e1ef67fbd23e57c43e8e83
21025290 <21025290@LAPTOP-PAHS8MMV>
2022-05-30 19:11:53 +0800

    Changes to CalculatorTest

--- L06/Test/CalculatorTest.java
+++ L06/Test/CalculatorTest.java
@@ -1,17 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
